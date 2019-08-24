efbf6e50644e0b9d7fb2a27d3457e976902f1751
Antti Eskelinen <antti.eskelinen@tuni.fi>
2019-08-24 19:32:45 +0300

    Fixed path in nav.bat

--- bin/nav.bat
+++ bin/nav.bat
@@ -1,6 +1,6 @@
 @echo off
 
-%NAV_HOME%\env\python.exe %NAV_HOME%\src\main.py %CD%
+%NAV_HOME%\env\windows-env\Scripts\python.exe %NAV_HOME%\src\main.py %CD%
 IF NOT ERRORLEVEL 1 (
     CALL %NAV_HOME%\tmp\navigate_to.bat 
     erase %NAV_HOME%\tmp\navigate_to.bat
