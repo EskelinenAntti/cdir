1894ce404274a459335f790bd604a718259487e4
Antti Eskelinen <antti.eskelinen3@gmail.com>
2020-04-28 09:56:44 +0300

    Updated setup.py and removed old bin

--- src/setup.py
+++ src/setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup, find_packages
 
-setup(name='nav', version='1.0', author="Antti Eskelinen", packages=find_packages())
+setup(name='nav',
+  version='1.0',
+  author="Antti Eskelinen",
+  scripts=['nav/bin/nav.ps1','nav/bin/nav.bat','nav/bin/nav.sh'],
+  packages=['nav', 'nav/data', 'nav/user_interface'],
+  entry_points={
+        "console_scripts": [
+            "nav_cli = nav",
+        ]
+    }
+  )
